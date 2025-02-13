b7eb7869b192db9907a36095f92fa812f5392e26
venkat-ecov-dev <ecovdev@ecovoice.ca>
2025-02-13 00:04:49 +0000

    Test header part.

--- src/utils/auth.rs
+++ src/utils/auth.rs
@@ -271,7 +271,7 @@ fn create_canonical_request(
     content_hash: &str,
 ) -> String {
     let decoded_path = percent_decode_str(path).decode_utf8().unwrap();
-    if content_hash == "UNSIGNED-PAYLOAD" {
+    if content_hash == "STREAMING-UNSIGNED-PAYLOAD-TRAILER" {
         return format!(
             "{}\n{}\n{}\n{}\n{}\n{}",
             method,
