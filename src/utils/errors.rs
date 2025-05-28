8375d5013a8e559cb2c365722859c70c709ebc68
Anthony Lukach <anthonylukach@gmail.com>
2025-05-28 14:33:30 -0700

    fix: handle unknown Rusoto errors with 404 status (#73)
    Added handling for unknown Rusoto errors that return a 404 status,
    mapping them to a custom `BackendError::ObjectNotFound`.

--- src/utils/errors.rs
+++ src/utils/errors.rs
@@ -180,6 +180,9 @@ impl From<RusotoError<HeadObjectError>> for BackendError {
             RusotoError::Service(HeadObjectError::NoSuchKey(e)) => {
                 BackendError::ObjectNotFound(Some(e))
             }
+            RusotoError::Unknown(e) if e.status == StatusCode::NOT_FOUND => {
+                BackendError::ObjectNotFound(Some(e.body_as_str().to_string()))
+            }
             _ => BackendError::S3Error(get_rusoto_error_message("HeadObject", error)),
         }
     }
