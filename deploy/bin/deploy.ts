0257bc0fb55487d7ff88edf8326fd7e8cda50191
Anthony Lukach <anthonylukach@gmail.com>
2025-08-05 22:27:28 -0700

    Add deployment tooling for api proxy

--- deploy/bin/deploy.ts
+++ deploy/bin/deploy.ts
@@ -4,16 +4,13 @@ import { DataProxyStack } from "../lib/data-proxy-stack";
 
 const stage = process.env.STAGE || "dev";
 
+const vpcId = process.env.VPC_ID;
+if (!vpcId) {
+  throw new Error("VPC_ID is not set");
+}
+
 const app = new cdk.App();
 new DataProxyStack(app, `DataProxy-${stage}`, {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  vpcId,
+  proxyDomain: `vercel-api-${stage}.internal`,
 });
--- deploy/lib/data-proxy-stack.ts
+++ deploy/lib/data-proxy-stack.ts
@@ -3,17 +3,22 @@ import { aws_ec2 as ec2 } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { VercelApiProxy } from "./vercel-api-proxy";
 
+interface DataProxyStackProps extends cdk.StackProps {
+  vpcId: string;
+  proxyDomain: string;
+}
+
 export class DataProxyStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: DataProxyStackProps) {
     super(scope, id, props);
 
     const vpc = ec2.Vpc.fromLookup(this, "Vpc", {
-      vpcId: "vpc-05858c6e5697bbc40",
+      vpcId: props.vpcId,
     });
 
     new VercelApiProxy(this, "EgressProxy", {
       vpc,
-      proxyDomain: "vercel-api.internal",
+      proxyDomain: props.proxyDomain,
     });
   }
 }
